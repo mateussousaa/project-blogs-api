9a4bb5327d43a9aafc83c9d5729b9415fb05e5d0
mateussousaa <mateussousa20@outlook.com>
2022-10-25 18:14:46 -0300

    feat: requirement 02 - completed

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { 
+      allowNull: true,
+      autoIncrement: true,
+      primaryKey: true, 
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tablename: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts,
+      { foreignKey: 'user_id', as: 'blog_posts'});
+  }
+
+  return User;
+}
